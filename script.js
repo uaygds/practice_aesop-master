681461c67501b0421a151e28e3f9007e6380f0f8
uaygds <nesh.oskar@gmail.com>
2023-04-11 04:03:38 +0300

    adaptive

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("burger").addEventListener("click", function(){
+        document.querySelector(".robot__header").classList.toggle('open')
+
+    });
+});
+
